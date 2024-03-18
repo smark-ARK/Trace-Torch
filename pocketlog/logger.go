0fab4c2df02cdc41dd52d13b49a20bde31b242de
smark-ark <arkhangreat2@gmail.com>
2024-03-18 15:46:35 +0530

    Added an initializer function

--- pocketlog/logger.go
+++ pocketlog/logger.go
@@ -1,4 +1,28 @@
 package pocketlog
 
 // Logger is used to log information.
-type Logger struct{}
+type Logger struct {
+	threshold Level
+}
+
+func New(threshold Level) *Logger {
+	return &Logger{
+		threshold: threshold,
+	}
+}
+
+func (l *Logger) Debugf(format string, args ...any) {
+	// Yet to be implemented
+}
+func (l *Logger) Infof(format string, args ...any) {
+	// Yet to be implemented
+}
+func (l *Logger) Warnf(format string, args ...any) {
+	// Yet to be implemented
+}
+func (l *Logger) Errorf(format string, args ...any) {
+	// Yet to be implemented
+}
+func (l *Logger) Fatalf(format string, args ...any) {
+	// Yet to be implemented
+}
